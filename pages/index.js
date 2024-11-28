928172191f4db1828e0aa5c62a79a644acb58510
Vinícius Marin <vmarin.10@gmail.com>
2024-11-28 18:36:32 +0000

    add initial page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      Estou recomeçando o curso.dev! Dessa vez vou até o final. Será que teremos
+      um final? Com certeza!
+    </h1>
+  );
+}
+
+export default Home;
